6f0f6f711053306a6c9b4163ca17003847745c5f
adigoswami <adigoswami007@gmail.com>
2023-05-31 13:53:48 -0600

    Add the use of tabs and newline

---
+++ Hello/tabs_spaces.py
@@ -0,0 +1,12 @@
+# Write a Python program to print the following string in a specific format:
+# Twinkle, twinkle, little star,
+#   How I wonder what you are!
+#       Up above the world so high,
+#       Like a diamond in the sky.
+#Twinkle, twinkle, little star,
+#   How I wonder what you are
+
+#This program gives you a simple idea how to use next line using '\n' and tab using '\t'
+#One can also use this to write patterns.
+
+print("Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\t How I wonder what you are!")
