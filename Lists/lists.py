ec6aeeb36011d481c3cbd1e5e07e95d4636b7874
adigoswami <adigoswami007@gmail.com>
2023-05-31 14:09:56 -0600

    Introduce lists

---
+++ Lists/lists.py
@@ -0,0 +1,15 @@
+#There are couple of ways to initialize lists.
+
+# Method 1: sqaure brackets
+lis = [5,6,8,9]
+print(lis)
+
+# Method 2: List() function to create an empty list only
+arr = list()
+print(arr)
+
+# Method 3: List comprehension 
+seq = [i for i in range(5)]
+print(seq)
+
+# We will further look at some more examples to understand list comprehension 
